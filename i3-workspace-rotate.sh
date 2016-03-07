36c56f09cb4f1abf4a749990f07abf2972f3bdeb
rickisen <rickard.spam@gmail.com>
2016-03-07 23:46:50 +0100

    working from terminal

--- i3-workspace-rotate.sh
+++ i3-workspace-rotate.sh
@@ -4,14 +4,19 @@ direction="$1"
 
 # find the active display (output)
 for (( i = 0; i < 10; i++ )); do
+
 	output=$( i3-msg -t get_workspaces | jq -c '.['"$i"'] | {output}' | sed -e 's?{"output":"??g' -e 's?"}??g' )
 	status=$( i3-msg -t get_workspaces | jq -c '.['"$i"'] | {focused}' | sed -e 's?{"focused":??g' -e 's?}??g')
+
 	if [[ $status == "true" ]] ; then
 		break
 	fi
 done
 
+# echo "trying to rotate display $output to $direction rotation"
 
-echo "rotating output is $output direction $direction"
+# Do it
+xrandr --output "$output" --rotate "$direction"
 
-
+# re render the wallpaper 
+~/.fehbg &
