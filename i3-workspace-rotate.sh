2ae14c152531ecc8634c5127f47754e530dd3d92
rickisen <rickard.spam@gmail.com>
2016-03-31 21:45:58 +0200

    added specific settings for my layout

--- i3-workspace-rotate.sh
+++ i3-workspace-rotate.sh
@@ -13,10 +13,26 @@ for (( i = 0; i < 10; i++ )); do
 	fi
 done
 
-# echo "trying to rotate display $output to $direction rotation"
+# specific to my setup
+if [[ "$output" == "DVI-I-1" ]]; then
+	if [[ "$direction" == "normal" ]]; then
+		options=" --mode 1920x1080 --rate 144 --right-of DVI-D-0 --output DVI-D-0 --auto "
+	else
+		options=" --mode 1920x1080 --rate 144 --right-of DVI-D-0 --output DVI-D-0 --auto "
+	fi
+elif [[ "$output" == "DP-0" ]]; then
+	options=" --right-of DVI-I-1 --output DVI-I-1 --auto "
+elif [[ "$output" == "DVI-D-0" ]]; then
+	options=" --left-of DVI-I-1 --output DVI-I-1 --auto "
+else
+	options=" "
+fi
 
-# Do it
-xrandr --output "$output" --rotate "$direction"
+# correct the height of middle display, Not optimal, should check width of left display
+if [[ "$direction" == "normal" && "$output" == "DVI-I-1" ]]; then
+	echo "xrandr --output "$output" --rotate normal --mode 1920x1080 --pos 1080x539 --rate 144" | sh -
+else
+	echo "xrandr --output "$output" --rotate "$direction" "$options"" | sh -
+fi
 
-# re render the wallpaper 
 ~/.fehbg &
